ab4467fdca3841ea12274261bf1abb3178246971
AliasPedroKarim <karimisma6464@gmail.com>
2022-03-14 00:44:30 +0100

    +1

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,43 +15,90 @@ var (
 	Token     = flag.String("token", "", "Provide Bot Token")
 	GuildID   = flag.String("guildtest", "", "Guild ID for test command slash.")
 	RemoveCmd = flag.Bool("rmcmd", true, "Remove command during shutting down bot.")
-
-	commands = []*discordgo.ApplicationCommand{}
 )
+
+var dgs *discordgo.Session
 
 func init() { flag.Parse() }
 
-func main() {
-	dg, err := discordgo.New("Bot " + *Token)
-	if err != nil {
-		fmt.Println("Error durring creating Discord Client Session: ", err)
-		return
+var (
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name:        "ping",
+			Description: "Check if bot is not die !!!",
+		},
 	}
-
-	dg.AddHandler(messageCreate)
-
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
-
-	err = dg.Open()
-	if err != nil {
-		fmt.Println("Error occured during open websocket client: ", err)
-		return
+	commandsHandler = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"ping": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			dgs.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: fmt.Sprintf("Great! I'm still alive, it seems! (%d ms)", dgs.HeartbeatLatency().Milliseconds()),
+				},
+			})
+		},
 	}
+)
 
-	fmt.Println("Bot " + dg.State.User.Username + " in running now. Press Ctrl + C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
-
-	dg.Close()
+func init() {
+	dgs.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandsHandler[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID || m.Author.Bot == true {
+func main() {
+	var err error
+	dgs, err = discordgo.New("Bot " + *Token)
+	if err != nil {
+		log.Println("Error durring creating Discord Client Session: ", err)
 		return
 	}
 
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong, i think i'm alive !!")
+	dgs.Identify.Intents = discordgo.IntentsGuildMessages
+
+	dgs.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logging as %v#%v in running now.", dgs.State.User.Username, dgs.State.User.Discriminator)
+	})
+
+	// dg.AddHandler(messageCreate)
+
+	err = dgs.Open()
+	if err != nil {
+		log.Fatalln("Error occured during open websocket client: ", err)
+		return
 	}
+
+	log.Println("Start to registering all commands...")
+	registeredCommand := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := dgs.ApplicationCommandCreate(dgs.State.User.ID, *GuildID, v)
+		if err != nil {
+			log.Panicf("Connot create '%v' command: %v", v.Name, err)
+			return
+		}
+
+		registeredCommand[i] = cmd
+	}
+
+	defer dgs.Close()
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	log.Println("Press Ctrl + C to exit.")
+	<-sc
+
+	if *RemoveCmd {
+		log.Print("Start to suppress all commands...")
+
+		for _, v := range registeredCommand {
+			err := dgs.ApplicationCommandDelete(dgs.State.User.ID, *GuildID, v.ID)
+			if err != nil {
+				log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+			}
+		}
+	}
+
+	log.Println("MakeFranceGreateAgain x3")
 }
