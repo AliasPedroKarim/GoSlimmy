b430f9681666c1756358a2c19779cce47279a7f4
AliasPedroKarim <karimisma6464@gmail.com>
2022-03-15 17:39:15 +0100

    start game tusmo

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -21,11 +22,57 @@ var dgs *discordgo.Session
 
 func init() { flag.Parse() }
 
+func init() {
+	var err error
+	dgs, err = discordgo.New("Bot " + *Token)
+	if err != nil {
+		log.Println("Error durring creating Discord Client Session: ", err)
+		return
+	}
+
+	dgs.Identify.Intents = discordgo.IntentsGuildMessages
+}
+
+const TUSMO_AMOUNT_TRY int = 6
+
+type TusmoPartyGame struct {
+	referenceWord  string
+	currentWord    []string
+	retryRemaining int
+	maxRound       int
+	Round          int
+}
+
 var (
+	tusmoMinAmount = 0.0
+
+	tusmoGameInProgress map[string]TusmoPartyGame = make(map[string]TusmoPartyGame)
+
 	commands = []*discordgo.ApplicationCommand{
 		{
 			Name:        "ping",
-			Description: "Check if bot is not die !!!",
+			Description: "Check if the bot isn't die !!!",
+		},
+		{
+			Name:        "tusmo",
+			Description: "The poor man's tusmo (it's a little bit stupid since tusmo is not even paid :joy:)",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Name:        "new-game",
+					Description: "Launched a new tusmo",
+					Options: []*discordgo.ApplicationCommandOption{
+						{
+							Name:        "number",
+							Description: "Maximum number of rounds you want to play",
+							MinValue:    &tusmoMinAmount,
+							MaxValue:    10,
+							Required:    false,
+							Type:        discordgo.ApplicationCommandOptionInteger,
+						},
+					},
+					Type: discordgo.ApplicationCommandOptionSubCommand,
+				},
+			},
 		},
 	}
 	commandsHandler = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
@@ -37,8 +84,87 @@ var (
 				},
 			})
 		},
+
+		"tusmo": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			switch i.ApplicationCommandData().Options[0].Name {
+			case "new-game":
+				// Error with "i.User.ID" because in here is "nil"
+
+				if _, ok := tusmoGameInProgress[i.Member.User.ID]; ok {
+					s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+						Type: discordgo.InteractionResponseChannelMessageWithSource,
+						Data: &discordgo.InteractionResponseData{
+							Content: "You've already started a game of Tusmo that's already in progress.",
+						},
+					})
+
+					return
+				}
+
+				var number int64 = 0
+				var content string
+
+				log.Println(i.ApplicationCommandData().Options[0])
+
+				if len(i.ApplicationCommandData().Options[0].Options) > 0 && i.ApplicationCommandData().Options[0].Options[0].Name == "number" {
+					number = i.ApplicationCommandData().Options[0].Options[0].IntValue()
+				}
+
+				if number == 0 {
+					content = fmt.Sprintf("You have just launched an **infinite** game!!! (well almost)")
+				} else {
+					content = fmt.Sprintf("You have just started a game of **%v** rounds.", number)
+				}
+
+				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: content,
+					},
+				})
+
+				var referenceWord = "Discord"
+				var a = strings.Split(referenceWord, "")
+
+				var currentWord = []string{a[0]}
+				for i := 1; i < len(a); i++ {
+					currentWord = append(currentWord, "\\_")
+				}
+
+				tusmoGameInProgress[i.Member.User.ID] = TusmoPartyGame{referenceWord, currentWord, TUSMO_AMOUNT_TRY, int(number), 1}
+
+				_, err := dgs.FollowupMessageCreate(dgs.State.User.ID, i.Interaction, true, &discordgo.WebhookParams{
+					Content: fmt.Sprintf(
+						"Round: %v --- Retry remaining: %v \n"+
+							"You have to guess the word: %v",
+						tusmoGameInProgress[i.Member.User.ID].Round,
+						tusmoGameInProgress[i.Member.User.ID].retryRemaining,
+						strings.Join(tusmoGameInProgress[i.Member.User.ID].currentWord, " | "),
+					),
+				})
+
+				if err != nil {
+					dgs.FollowupMessageCreate(dgs.State.User.ID, i.Interaction, true, &discordgo.WebhookParams{
+						Content: "Something went wrong",
+					})
+					return
+				}
+
+				break
+			default:
+				log.Println("[Tusmo] >> BAH rien !!")
+			}
+
+		},
 	}
 )
+
+func messageCreate(s *discordgo.Session, i *discordgo.MessageCreate) {
+	// Check if player
+	if _, ok := tusmoGameInProgress[i.Member.User.ID]; ok {
+		// s.ChannelMessageSend(i.ChannelID, )
+	}
+}
 
 func init() {
 	dgs.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
@@ -49,22 +175,11 @@ func init() {
 }
 
 func main() {
-	var err error
-	dgs, err = discordgo.New("Bot " + *Token)
-	if err != nil {
-		log.Println("Error durring creating Discord Client Session: ", err)
-		return
-	}
-
-	dgs.Identify.Intents = discordgo.IntentsGuildMessages
-
 	dgs.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
 		log.Printf("Logging as %v#%v in running now.", dgs.State.User.Username, dgs.State.User.Discriminator)
 	})
 
-	// dg.AddHandler(messageCreate)
-
-	err = dgs.Open()
+	err := dgs.Open()
 	if err != nil {
 		log.Fatalln("Error occured during open websocket client: ", err)
 		return
